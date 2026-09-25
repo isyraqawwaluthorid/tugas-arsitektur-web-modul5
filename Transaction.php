6dc6c27013ca3d4877d200839f9f20de8a208753
isyraqawwaluthorid <isyraqawwal7@gmail.com>
2026-09-25 18:55:02 +0800

    Tambahkan metode process dengan validasi saldo dan ekspresi match

--- Transaction.php
+++ Transaction.php
@@ -25,4 +25,23 @@ class Transaction
     {
         return $this->amount;
     }
+
+    public function process(): bool
+    {
+        $balance = (float) ($_SESSION['balance'] ?? 0.0);
+
+        $newBalance = match ($this->type) {
+            'deposit' => $balance + $this->amount,
+            'penarikan' => $balance - $this->amount,
+            default => throw new InvalidArgumentException("Jenis transaksi tidak dikenal: {$this->type}"),
+        };
+
+        if ($this->type === 'penarikan' && $newBalance < 0) {
+            return false;
+        }
+
+        $_SESSION['balance'] = $newBalance;
+
+        return true;
+    }
 }
